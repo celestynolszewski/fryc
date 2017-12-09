9bd7bbd8e7808d416514bb1f9987127ac457f563
celestynolszewski <34186304+celestynolszewski@users.noreply.github.com>
2017-12-09 20:35:21 +0100

    Add files via upload

---
+++ external.php
@@ -0,0 +1,133 @@
+<?php
+
+  function logwrite($string,$handler) {
+      echo $string.PHP_EOL;
+      if($handler) {
+        fwrite($handler,$string.PHP_EOL);     
+      }
+  }
+
+  // domena -> mail server. mozna to zrobic przez odpytywanie dns o rekord mx ale dodaloby jedynie dodatkowe opoznienie
+  // przeoranie pliku bez ladowania do tablicy wydajnosciowo tez nie jest wcale takie zle bo ponizej 0.0001 sekundy
+  function getHost ($email) {
+
+      $split = explode("@",$email);
+      $domain = $split[1];
+            
+    // przetwarzanie pliku servers.txt
+    (file_exists("servers.txt") && filesize("servers.txt")>0 ? $file_servers = fopen("servers.txt","r") : $file_servers = false);
+
+     if($file_servers) {
+        $servers = array();
+        while(($s_line = fgets($file_servers)) !== FALSE) {
+             $servers = explode(";",$s_line);
+             
+             if(array_search($domain, $servers)) $imap=$servers[0]; 
+             
+        }
+      } else die('Brak pliku servers.txt! Bez niego nie podzialam :(');
+  
+      if($file_servers) fclose($file_servers);
+     
+      return $imap;   
+  }
+  
+class Zipper extends ZipArchive {
+    public function addDir($path, $parent_dir = '') {
+        if($parent_dir != ''){
+            $this->addEmptyDir($parent_dir);
+            $parent_dir .= '/';
+           // print '<br>adding dir ' . $parent_dir . '<br>';
+        }
+        $nodes = glob($path . '/*');
+        foreach ($nodes as $node) {
+            if (is_dir($node)) {
+                $this->addDir($node, $parent_dir.basename($node));
+            }
+            else if (is_file($node))  {
+                $this->addFile($node, $parent_dir.basename($node));
+                //print 'adding file '.$parent_dir.basename($node) . '<br>';
+            }
+        }
+    }
+}
+
+function deleteDir($path)
+{
+    return !empty($path) && is_file($path) ?
+        @unlink($path) :
+        (array_reduce(glob($path.'/*'), function ($r, $i) { return $r && deleteDir($i); }, TRUE)) && @rmdir($path);
+}
+
+
+
+class mail {
+    /**
+      * If you change one of these, please check the other for fixes as well
+     *
+     * @const Pattern to match RFC 2047 charset encodings in mail headers
+     */
+    const rfc2047header = '/=\?([^ ?]+)\?([BQbq])\?([^ ?]+)\?=/';
+
+    const rfc2047header_spaces = '/(=\?[^ ?]+\?[BQbq]\?[^ ?]+\?=)\s+(=\?[^ ?]+\?[BQbq]\?[^ ?]+\?=)/';
+
+    /**
+     * http://www.rfc-archive.org/getrfc.php?rfc=2047
+     *
+     * =?<charset>?<encoding>?<data>?=
+     *
+     * @param string $header
+     */
+    public static function is_encoded_header($header) {
+        // e.g. =?utf-8?q?Re=3a=20Support=3a=204D09EE9A=20=2d=20Re=3a=20Support=3a=204D078032=20=2d=20Wordpress=20Plugin?=
+        // e.g. =?utf-8?q?Wordpress=20Plugin?=
+        return preg_match(self::rfc2047header, $header) !== 0;
+    }
+
+    public static function header_charsets($header) {
+        $matches = null;
+        if (!preg_match_all(self::rfc2047header, $header, $matches, PREG_PATTERN_ORDER)) {
+            return array();
+        }
+        return array_map('strtoupper', $matches[1]);
+    }
+
+    public static function decode_header($header) {
+        $matches = null;
+
+        /* Repair instances where two encodings are together and separated by a space (strip the spaces) */
+        $header = preg_replace(self::rfc2047header_spaces, "$1$2", $header);
+
+        /* Now see if any encodings exist and match them */
+        if (!preg_match_all(self::rfc2047header, $header, $matches, PREG_SET_ORDER)) {
+            return $header;
+        }
+        foreach ($matches as $header_match) {
+            list($match, $charset, $encoding, $data) = $header_match;
+            $encoding = strtoupper($encoding);
+            switch ($encoding) {
+                case 'B':
+                    $data = base64_decode($data);
+                    break;
+                case 'Q':
+                    $data = quoted_printable_decode(str_replace("_", " ", $data));
+                    break;
+                default:
+                    throw new Exception("preg_match_all is busted: didn't find B or Q in encoding $header");
+            }
+            // This part needs to handle every charset
+            switch (strtoupper($charset)) {
+                case "UTF-8":
+                    break;
+                default:
+                break;
+                    /* Here's where you should handle other character sets! */
+                  //  throw new Exception("Unknown charset in header - time to write some code.");
+            }
+            $header = str_replace($match, $data, $header);
+        }
+        return $header;
+    }
+}
+
+?>
